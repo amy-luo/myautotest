b6b9ba160688cb7a39070486d863338b57067444
amy_luo <luoyafang@163.com>
2020-07-30 21:49:58 +0800

    push

--- src/test/java/com/mytest/function/Utils/PoiWriteUtil.java
+++ src/test/java/com/mytest/function/Utils/PoiWriteUtil.java
@@ -6,27 +6,29 @@ import java.io.IOException;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class PoiWriteUtil {
-        public static void main(String[] args) throws IOException
+        public void poiWrite(String filePath) throws IOException
         {
             // 创建工作薄
-            HSSFWorkbook workbook = new HSSFWorkbook();
+            XSSFWorkbook workbook = new XSSFWorkbook();
             // 创建工作表
-            HSSFSheet sheet = workbook.createSheet("sheet1");
+            XSSFSheet sheet = workbook.createSheet("sheet1");
 
             for (int row = 0; row < 10; row++)
             {
-                HSSFRow rows = sheet.createRow(row);
+                XSSFRow rows = sheet.createRow(row);
                 for (int col = 0; col < 10; col++)
                 {
                     // 向工作表中添加数据
                     rows.createCell(col).setCellValue("data" + row + col);
                 }
             }
-
-            File xlsFile = new File("poi.xls");
-            FileOutputStream xlsStream = new FileOutputStream(xlsFile);
-            workbook.write(xlsStream);
+            File file=new File(filePath);
+            FileOutputStream fos = new FileOutputStream(file);
+            workbook.write(fos);
         }
 }
