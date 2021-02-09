f4da4ace3e6fae70731d60f4ae569adf3ee0e5d7
amy_luo <luoyafang@163.com>
2021-02-10 00:22:58 +0800

    修改压测平台框架

--- pt-worker/pt-worker-base/src/main/java/com/mytest/ptworker/base/PtWorkerApplication.java
+++ pt-worker/pt-worker-base/src/main/java/com/mytest/ptworker/base/PtWorkerApplication.java
@@ -1,12 +1,13 @@
 package com.mytest.ptworker.base;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
 /**
  * Hello world!
  *
  */
 
 @SpringBootApplication
-
 public class PtWorkerApplication
 {
     public static void main( String[] args ) {
---
+++ pt-worker/pt-worker-base/src/main/java/com/mytest/ptworker/base/service/impl/WorkerQueryServiceImpl.java
@@ -0,0 +1,14 @@
+package com.mytest.ptworker.base.service.impl;
+
+import com.mytest.ptworker.client.model.dto.WorkerStatusDTO;
+import com.mytest.ptworker.client.service.WorkExecService;
+import com.mytest.ptworker.client.service.WorkerQueryService;
+
+//服务层的服务实现
+//TODO 接入RPC框架的provider注解
+public class WorkerQueryServiceImpl implements WorkerQueryService {
+    @Override
+    public WorkerStatusDTO queryWorkStatus() {
+        return null;
+    }
+}
